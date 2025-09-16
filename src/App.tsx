c6262df049666f1ab11eba3811942e87aeb2140f
Andrew Lemons <github@aglemons.com>
2025-09-15 20:47:58 -0400

    Improve theming

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import StartCard from "./components/start-card";
 import { useSolutions } from "./hooks/solutions";
 import ValidSolutionCard from "./components/valid-solution-card";
 import InvalidSolutionCard from "./components/invalid-solution-card";
+import Logo from "./logo.svg";
+import { Card } from "./components/ui/card";
 
 export function App() {
 	const { gameState } = useGameState();
@@ -38,7 +40,21 @@ export function App() {
 	// All clue logic handled within dedicated components; this root orchestrates high-level card display.
 
 	return (
-		<div className="container mx-auto px-4 py-8 text-center relative max-w-xl">
+		<div className="container mx-auto px-4 py-8 text-center relative max-w-xl flex flex-col">
+			<img src={Logo} alt="Hive Hunt Logo" className="w-full h-16 p-4" />
+			{
+				/* Defauly content when no game state is active */
+				!gameState.hasStarted && !allowStart && (
+					<Card className="bg-card/50 backdrop-blur-sm border-muted p-6">
+						<h1 className="text-4xl font-bold mb-4">404 Not Found</h1>
+						<p className="mb-4">The page you are looking for does not exist.</p>
+						<p className="text-sm text-muted-foreground">
+							If you belive this is an error, please contact an event
+							coordinator.
+						</p>
+					</Card>
+				)
+			}
 			{showIsValidSolution && (
 				<ValidSolutionCard onClose={() => setShowIsValidSolution(false)} />
 			)}
